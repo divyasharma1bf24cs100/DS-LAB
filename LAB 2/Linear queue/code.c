5b7b35a13136eb08a167fe6803c2159b511f173f
divyasharma1bf24cs100 <divyasharma.cs24@bmsce.ac.in>
2025-10-16 12:00:07 +0530

    Update code.c

--- LAB 2/Linear queue/code.c
+++ LAB 2/Linear queue/code.c
@@ -1,70 +1,71 @@
 #include<stdio.h>
 #define SIZE 5
 int queue[SIZE];
-int front=-1,rear=-1;
-void insert(){
+int front = -1, rear = -1;
+
+void insert() {
     int item;
-    if(rear==SIZE-1){
-        printf("Overflow!");
+    if (rear == SIZE - 1) {
+        printf("Overflow!\n");
         return;
-    }
-    else{
-        printf("Enter element to be inserted:");
-        scanf("%d",&item);
-        if(front==-1){
-            front=0;
+    } else {
+        printf("Enter element to be inserted: ");
+        scanf("%d", &item);
+        if (front == -1) {
+            front = 0;
         }
-        else{
-            rear++;
-            queue[rear]=item;
-            printf("%d has been inserted",item);
-        }
+        rear++;
+        queue[rear] = item;
+        printf("%d has been inserted\n", item);
     }
 }
-void delete(){
-    int item;
-    if(front==rear||front>rear){
-        printf("Underflow!");
+
+void delete() {
+    if (front == -1 || front > rear) {
+        printf("Underflow!\n");
         return;
-    }
-    else{
-        printf("%d has been deleted",queue[front]);
-        front++;
-        if(front>rear){
-            front=rear-1;
+    } else {
+        printf("%d has been deleted\n", queue[front]);
+        if (front == rear) {
+            front = -1;
+            rear = -1;
+        } else {
+            front++;
         }
     }
 }
-void display(){
-    if(front==-1){
-        printf("Queue is empty!");
+
+void display() {
+    if (front == -1) {
+        printf("Queue is empty!\n");
         return;
-    }
-    else{
-        printf("Queue elements are:");
-        for(int i=front;i<=rear;i++){
-            printf("%d\n",queue[i]);
-        }printf("\n");
+    } else {
+        printf("Queue elements are: ");
+        for (int i = front; i <= rear; i++) {
+            printf("%d ", queue[i]);
+        }
+        printf("\n");
     }
 }
-int main(){
+
+int main() {
     int choice;
-    do{
-        printf("Linear Queue Operations:");
-        printf("1.Insert\n2.Delete\n3.Display\n4.Exit\n");
-        printf("Enter choice:");
-        scanf("%d",&choice);
-        switch(choice){
-        case 1: insert();
-                break;
-        case 2: delete();
-                break;
-        case 3: display();
-                break;
-        case 4: printf("Exiting!");
-                break;
-        default: printf("Invalid choice!");
+    do {
+        printf("\nLinear Queue Operations:\n");
+        printf("1. Insert\n2. Delete\n3. Display\n4. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1: insert();
+                    break;
+            case 2: delete();
+                    break;
+            case 3: display();
+                    break;
+            case 4: printf("Exiting!\n");
+                    break;
+            default:printf("Invalid choice!\n");
         }
-    }while(choice!=4);
+    } while (choice != 4);
     return 0;
 }
