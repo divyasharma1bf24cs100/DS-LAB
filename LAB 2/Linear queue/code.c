e4c3a3d86d6fea1463855ef4b85f1ebde51d062e
divyasharma1bf24cs100 <divyasharma.cs24@bmsce.ac.in>
2025-10-16 11:48:01 +0530

    Create code.c

---
+++ LAB 2/Linear queue/code.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#define SIZE 5
+int queue[SIZE];
+int front=-1,rear=-1;
+void insert(){
+    int item;
+    if(rear==SIZE-1){
+        printf("Overflow!");
+        return;
+    }
+    else{
+        printf("Enter element to e inserted:");
+        scanf("%d",&item);
+        if(front==-1){
+            front=0;
+        }
+        else{
+            rear++;
+            queue[rear]=item;
+            printf("%d has been inserted",item);
+        }
+    }
+}
+void delete(){
+    int item;
+    if(front==rear||front>rear){
+        printf("Underflow!");
+        return;
+    }
+    else{
+        printf("%d has been deleted",queue[front]);
+        front++;
+        if(front>rear){
+            front=rear-1;
+        }
+    }
+}
+void display(){
+    if(front==-1){
+        printf("Queue is empty!");
+        return;
+    }
+    else{
+        printf("Queue elements are:");
+        for(int i=front;i<=rear;i++){
+            printf("%d\n",queue[i]);
+        }printf("\n");
+    }
+}
+int main(){
+    int choice;
+    do{
+        printf("Linear Queue Operations:");
+        printf("1.Insert\n2.Delete\n3.Display\n4.Exit\n");
+        printf("Enter choice:");
+        scanf("%d",&choice);
+        switch(choice){
+        case 1: insert();
+                break;
+        case 2: delete();
+                break;
+        case 3: display();
+                break;
+        case 4: printf("Exiting!");
+                break;
+        default: printf("Invalid choice!");
+        }
+    }while(choice!=4);
+    return 0;
+}
